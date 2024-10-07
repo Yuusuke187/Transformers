6a4a096c4385ff46791b52fbb8cf4e883f89acb7
Paxton Perry <paxton.perry@protonmail.com>
2024-10-07 15:16:32 -0700

    Creating a summarization script

---
+++ NLP/DocSum.py
@@ -0,0 +1,19 @@
+# This script will be used for Document Summarization
+
+from transformers import pipeline
+
+# Load a pre-trained summarization model from Facebook
+summarizer = pipeline("summarization", \
+    model="facebook/bart-large-cnn")
+
+# Create a long string to go in a document
+long_text = """
+AI has shown remarkable progress since the early days of the internet.
+It has revolutionized the way we interact with information.
+It has made it easy to create and share data.
+It has changed the way we think about healthcare.
+It has made healthcare more accessible.
+It has changed the way we live.
+(This was all generated with Codeium)
+"""
+
