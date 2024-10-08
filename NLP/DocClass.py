f822f15433a0dac3ebc89223ddcefa9a24934ec4
Paxton <paxton.perry@protonmail.com>
2024-10-07 22:24:31 -0700

    Added classifier code

--- NLP/DocClass.py
+++ NLP/DocClass.py
@@ -4,4 +4,8 @@ from transformers import pipeline
 
 # Load a pre-trained classification model from Hugging Face
 # Let's use distilbert-base-uncased-finetuned-sst-2-english
-classifier = pipeline("text-classification", model="distilbert-base-uncased-finetuned-sst-2-english")
+classifier = pipeline("text-classification", \
+    model="distilbert-base-uncased-finetuned-sst-2-english", \
+    device=0)
+
+
