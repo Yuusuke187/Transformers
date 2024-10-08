8e31bbf2ff45274e34c130a7f5e5b84dbe98227a
Paxton <paxton.perry@protonmail.com>
2024-10-07 22:48:44 -0700

    Made the classification code a function and removed the hard coded document text

--- NLP/DocClass.py
+++ NLP/DocClass.py
@@ -2,18 +2,21 @@
 
 from transformers import pipeline
 
-# Load a pre-trained classification model from Hugging Face
-# Let's use distilbert-base-uncased-finetuned-sst-2-english
-classifier = pipeline("text-classification", \
-    model="distilbert-base-uncased-finetuned-sst-2-english", \
-    device=0)
+def classify_text(document_text):
+    # Load a pre-trained classification model from Hugging Face
+    # Let's use distilbert-base-uncased-finetuned-sst-2-english
+    classifier = pipeline("text-classification", \
+        model="distilbert-base-uncased-finetuned-sst-2-english", \
+        device=0)
 
-document_text = """
-Raiden is a vertically scrolling shooting game (shmup) released by Seibu Kaihatsu in 1990.
-"""
+    # document_text = """
+    # Raiden is a vertically scrolling shooting game (shmup) released by Seibu Kaihatsu in 1990.
+    # """
+    if document_text == "":
+        document_text = input("Enter the text that will be clasified: ")
 
-# Classify the text
-classification = classifier(document_text)
+    # Classify the text
+    classification = classifier(document_text)
 
-# Print out the classification
-print(classification)
+    # Print out the classification
+    print(classification)
