b7d4d20e1226d82316fd2f16b967d2c2c1572748
Paxton <paxton.perry@protonmail.com>
2024-10-09 19:32:26 -0700

    Finished the function to list the stop words

--- Stopwords/ListStopWords.py
+++ Stopwords/ListStopWords.py
@@ -1,1 +1,16 @@
+# Stop words are words that are of little or no value in Natural
+# Language processing.
+
 import nltk
+
+
+def list_stop_words():
+    nltk.download('stopwords')
+    stop_words = nltk.corpus.stopwords.words('english')
+    
+    print("Stop words count: ", len(stop_words))
+    print("Getting the first seven stop words in the list: ", stop_words[:7])
+    
+    print("Returning the stopwords that only consist of one letter...")
+    [sw for sw in stop_words if len(sw) == 1]
+    
